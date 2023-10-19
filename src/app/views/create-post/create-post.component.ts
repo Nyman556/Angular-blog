e52c9894fac7e5d0691676433f5896da0399e1c5
Johan Nyman <johan.nyman97@gmail.com>
2023-10-19 19:57:18 +0200

    create post sektion

--- src/app/views/create-post/create-post.component.ts
+++ src/app/views/create-post/create-post.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
-  styleUrls: ['./create-post.component.css']
+  styleUrls: ['./create-post.component.css'],
 })
 export class CreatePostComponent {
-
+  constructor(private PostService: PostService) {}
 }
